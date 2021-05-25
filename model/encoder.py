692b55de7a6fdf05edc7bbfbf4b83659e9d4e6b8
amolero6 <molerator@gmail.com>
2021-05-25 12:01:09 +0200

    Adapted device and first inference tests

--- model/encoder.py
+++ model/encoder.py
@@ -2,10 +2,11 @@ from torch.functional import Tensor
 import torchvision.models as models
 import torch.nn as nn
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self,device):
         super(Encoder, self).__init__()
-        pretrained_model = models.vgg16(pretrained=True)
-        self.conv_base = pretrained_model.features
+        print("Extracting feature vectors from the image")
+        pretrained_model = models.vgg16(pretrained=True).to(device)
+        self.conv_base = pretrained_model.features.to(device)
         # Freeze All layers as they will be used for inference
         for _, param in enumerate(self.conv_base.parameters()):  
             param.requires_grad = False
