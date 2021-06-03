6238070973c8ccf2cfd4816f0517bed785ab38c5
amolero6 <molerator@gmail.com>
2021-06-03 08:26:54 +0200

    Working evaluate function

---
+++ evaluate.py
@@ -0,0 +1,45 @@
+import torch
+import nltk
+from torch.nn.functional import pad
+from torch.nn.utils.rnn import pad_packed_sequence, pad_sequence
+from nltk.translate.bleu_score import corpus_bleu
+from model.visualization import Visualization
+
+
+def evaluate(model, test_loader, vocab, device, criterion):  # TODO:add device
+	model.eval()
+
+	total_loss = 0.
+
+	
+
+	with torch.no_grad():
+		for idx, batch in enumerate(iter(test_loader)):
+			img, target = batch
+			img = img.to(device)
+			target = target.to(device)
+			sentence_s = []
+			target_s = []
+			sentences = []
+			attention_w = []
+			for i in range(img.shape[0]):
+				sentence, attention_w_t = model.inference(image=img[i].unsqueeze(0), vocab=vocab)
+				sentence_s=sentence.split(' ')
+				sentence_s.append('<END>')
+				sentences.append(sentence_s)
+				target_s.append(vocab.generate_caption(target[i,1:]).split(' '))
+				attention_w.append(attention_w_t)
+				
+			total_loss += corpus_bleu(target_s,sentences,(1.0/1.0,))
+			
+
+			if idx % 10 == 0:
+				example=' '.join(sentences[5])
+				print(f'Evaluating batch {idx} / {len(test_loader)}...')
+				print(f'Gen example: {example}')
+				print(f'Exp example: {vocab.generate_caption(target[5,1:])}')
+				#Visualization.show_image(img[idx],title=' '.join(sentences[idx]))
+				# Visualization.plot_attention(img[idx],sentences[idx],attention_w[idx])
+				pass
+
+		return total_loss / (idx+1)
--- train.py
+++ train.py
@@ -2,8 +2,9 @@ import time
 import torch
 import math
 import os
-from model.visualization import Visualization
-from torchtext.data.metrics import bleu_score
+
+from evaluate import evaluate
+
 def train_single_epoch(epoch, model, train_loader, optimizer, criterion, device, log_interval):
 	"""
 	Train single epoch
@@ -29,39 +30,16 @@ def train_single_epoch(epoch, model, train_loader, optimizer, criterion, device,
 
 		total_loss += loss.item()
 
-		print('-'*89)
-		print(f'| epoch {epoch:3d} | {i:5d}/{len(train_loader):5d} batches | loss {total_loss:5.2f} | ppl {math.exp(total_loss):8.2f}')
-		print('-'*89)
-		print(f'Gen: {model.vocab.generate_caption(torch.argmax(output[0].transpose(1,0), dim=-1))}')
-		print(f'Exp: {model.vocab.generate_caption(target[0,1:])}')
-		print('-'*89)
-		candidate_corpus = [model.vocab.generate_caption(torch.argmax(output[0].transpose(1, 0), dim=-1))]
-		reference_corpus = [model.vocab.generate_caption(target[0, 1:])]
-		print(f'Bleu score: {bleu_score(candidate_corpus, reference_corpus)}')
+		if i % 10==0:
+			print('-'*89)
+			print(f'| epoch {epoch:3d} | {i:5d}/{len(train_loader):5d} batches | loss {total_loss:5.2f} | ppl {math.exp(total_loss):8.2f}')
+			print('-'*89)
+			print(f'Gen: {model.vocab.generate_caption(torch.argmax(output[0].transpose(1,0), dim=-1))}')
+			print(f'Exp: {model.vocab.generate_caption(target[0,1:])}')
+			print('-'*89)
 		total_loss = 0.
 
-def evaluate(model,test_loader, vocab, device,criterion):#TODO:add device
-	model.eval()
 
-	total_loss = 0.
-	sentences = []
-	#device= 'cpu'
-	with torch.no_grad():
-		for idx, batch in enumerate(iter(test_loader)):
-			img, target = batch
-			img = img.to(device)
-			target = target.to(device)
-			for i in range(img.shape[0]):
-				sentence = model.inference(image=img[i].unsqueeze(0),vocab=vocab)
-				alphas = model.forward(image=img[i].unsqueeze(0), vocab=vocab)[1]
-			
-
-			caption = ' '.join(sentence)
-			Visualization.plot_attention((img[0]), sentence, alphas) # showing expected and plotting attention
-			total_loss += target.numel()*criterion(sentence,target).item()
-			n += target.numel()
-
-		return total_loss / n, caption
 
 
 def save_model(model, epoch):
@@ -75,22 +53,24 @@ def save_model(model, epoch):
 	}
 	torch.save(model_state, filename)
 
-def train(num_epochs, model, train_loader,test_loader, optimizer, criterion, device,log_interval):
+def train(num_epochs, model, train_loader,test_loader, optimizer, criterion, device,log_interval,vocab):
 	"""
 	Executes model training. Saves model to a file every epoch.
 	"""	
 
 	for epoch in range(1,num_epochs+1):
-
-		train_single_epoch(epoch, model, train_loader,optimizer, criterion, device, log_interval)
+		epoch_start_time=time.time()
+		train_single_epoch(epoch=epoch,model=model,train_loader=train_loader,optimizer=optimizer, criterion=criterion,device= device,log_interval=log_interval)
 
 		if epoch % 5 == 0:
 			save_model(model, epoch)
 		
-		# val_loss, _ = evaluate(model,test_loader,vocab,device,criterion)
+		val_loss = evaluate(model=model,test_loader=test_loader,vocab=vocab,device=device,criterion=criterion)
 
-		# print('-' * 89)
-		# print(f'| end of epoch {epoch} | time: {(time.time() - epoch_start_time):.2f}s | valid loss {val_loss:.2f}')
-		# print('-' * 89)
+		print('-' * 89)
+		print(f'| end of epoch {epoch} | time: {(time.time() - epoch_start_time):.2f}s | valid loss {val_loss:.2f}')
+		print('-' * 89)
+
+
 
 		
