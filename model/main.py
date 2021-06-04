3019c091836c336d1ef4c1e00dac05073e76a612
amolero6 <66525598+amolero6@users.noreply.github.com>
2021-06-04 11:26:47 +0200

    Working evaluate function (#25)
    * Working evaluate function
    
    * Working evaluate function
    
    * Enable visualization
    
    * Added fixes from Lucas comments
    
    * Fix bug with vocab
    
    * Fix bug in visualiization grid
    
    * Make plots from random image

--- model/main.py
+++ model/main.py
@@ -38,22 +38,27 @@ class ImageCaptioningModel(nn.Module):
 			attention_weights.append(alphas)
 		return predictions.permute(1,2,0), attention_weights
 	
-	def inference(self, image, vocab:Vocabulary):
+	def inference(self, image):
 		image_features = self.encoder(image)
 		hidden = self.decoder.init_hidden(image.shape[0])
 		
 		if torch.cuda.is_available():
-			word = torch.cuda.IntTensor([vocab.word_to_index['<START>']])
+			word = torch.cuda.IntTensor([self.vocab.word_to_index['<START>']])
 		else:
-			word = torch.tensor(vocab.word_to_index['<START>'])
-		sentence = [word.tolist()]
-		
+			word = torch.tensor(self.vocab.word_to_index['<START>'])
+		sentence = [word.item()]
+		attention_weights=[]
 		for i in range(self.caption_max_length):
 			alphas, weighted_features = self.attention.forward(image_features, hidden)
 			predictions_t, hidden = self.decoder.forward(weighted_features, word, hidden)
 			word = torch.argmax(predictions_t, dim=-1)
-			sentence.append(word.tolist())
-			
-			if word[0].tolist()==vocab.word_to_index['<END>']:
+			if word[0].item()==self.vocab.word_to_index['<END>']:
 				break
-		return sentence
+			sentence.append(word.item())
+			attention_weights.append(alphas)
+		if torch.cuda.is_available():
+			sentence=torch.cuda.IntTensor(sentence[1:])
+		else:
+			sentence=torch.tensor(sentence[1:])
+		sentence=self.vocab.generate_caption(sentence)
+		return sentence, attention_weights
